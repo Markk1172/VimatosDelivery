cfacbd8ad16e691f41c811a95190c8dea8ccb09d
SmankyR <gabrieldbz17@gmail.com>
2025-05-19 11:55:58 -0300

    services & portfolio

--- frontend1/src/components/Portfolio.jsx
+++ frontend1/src/components/Portfolio.jsx
@@ -9,40 +9,40 @@ import React from "react";
 
 const portfolioItems = [
   {
-    title: "Threads",
-    subtitle: "Illustration",
+    title: "Calabresa",
+    subtitle: "Classica, com cebola",
     img: "/assets/img/portfolio/1.jpg",
     modal: "#portfolioModal1",
   },
   {
-    title: "Explore",
-    subtitle: "Graphic Design",
+    title: "Portuguesa",
+    subtitle: "Com ovos, presunto e muito pimentão",
     img: "/assets/img/portfolio/2.jpg",
     modal: "#portfolioModal2",
   },
   {
-    title: "Finish",
-    subtitle: "Identity",
+    title: "Muçarela",
+    subtitle: "Bem queijuda, oréganoza :)",
     img: "/assets/img/portfolio/3.jpg",
     modal: "#portfolioModal3",
   },
   {
-    title: "Finish",
-    subtitle: "Identity",
+    title: "Coca-Cola",
+    subtitle: "A inconfundível",
     img: "/assets/img/portfolio/4.jpg",
-    modal: "#portfolioModal3",
+    modal: "#portfolioModal4",
   },
   {
-    title: "Finish",
-    subtitle: "Identity",
-    img: "/assets/img/portfolio/5.jpg",
-    modal: "#portfolioModal3",
+    title: "Chocolate",
+    subtitle: "Saborosa e doce",
+    img: "/assets/img/portfolio/5.png",
+    modal: "#portfolioModal5",
   },
   {
-    title: "Finish",
-    subtitle: "Identity",
+    title: "Frango e Catupiry",
+    subtitle: "Deliciosa combinação",
     img: "/assets/img/portfolio/6.jpg",
-    modal: "#portfolioModal3",
+    modal: "#portfolioModal6",
   },
   // Adicione mais itens conforme necessário
 ];
@@ -52,8 +52,8 @@ function Portfolio() {
     <section className="page-section bg-light" id="portfolio">
       <div className="container">
         <div className="text-center">
-          <h2 className="section-heading text-uppercase">Portfolio</h2>
-          <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+          <h2 className="section-heading text-uppercase">Cardapio</h2>
+          <h3 className="section-subheading text-muted">Nossos campeões de vendas!</h3>
         </div>
         <div className="row">
           {portfolioItems.map((item, idx) => (
@@ -65,7 +65,7 @@ function Portfolio() {
                       <i className="fas fa-plus fa-3x"></i>
                     </div>
                   </div>
-                  <img className="img-fluid" src={item.img} alt={item.title} />
+                  <img className="img-fluid" src={item.img} alt={item.title}   style={{ width: "100%", height: "250px", objectFit: "cover" }} />
                 </a>
                 <div className="portfolio-caption">
                   <div className="portfolio-caption-heading">{item.title}</div>
