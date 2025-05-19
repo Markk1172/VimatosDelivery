cc26eeacd81ffe994deb32fe4db0bf3ecaf57c16
SmankyR <gabrieldbz17@gmail.com>
2025-05-19 10:02:21 -0300

    começando as mudancas no front

--- frontend1/src/components/Services.jsx
+++ frontend1/src/components/Services.jsx
@@ -6,17 +6,18 @@ function Services() {
         <section className="page-section" id="services">
             <div className="container">
                 <div className="text-center">
-                    <h2 className="section-heading text-uppercase">Services</h2>
+                    <h2 className="section-heading text-uppercase">Serviços</h2>
                     <h3 className="section-subheading text-muted">
-                        Lorem ipsum dolor sit amet consectetur.
+                        Pizzas deliciosas, feitas com amor e carinho. <br />
                     </h3>
                 </div>
                 <div className="row text-center">
                     <div className="col-md-4">
                         <span className="fa-stack fa-4x">
-                            <i className="fas fa-circle fa-stack-2x text-primary"></i>
-                            <i className="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
-                        </span>
+                        <i className="fas fa-circle fa-stack-2x text-danger"></i> {/* fundo vermelho */}
+                        <i className="fas fa-pizza-slice fa-stack-1x fa-inverse"></i> {/* ícone branco */}
+                    </span>
+
                         <h4 className="my-3">E-Commerce</h4>
                         <p className="text-muted">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.
@@ -24,8 +25,8 @@ function Services() {
                     </div>
                     <div className="col-md-4">
                         <span className="fa-stack fa-4x">
-                            <i className="fas fa-circle fa-stack-2x text-primary"></i>
-                            <i className="fas fa-laptop fa-stack-1x fa-inverse"></i>
+                            <i className="fas fa-circle fa-stack-2x text-danger"></i>
+                            <i className="fas fa-wine-glass fa-stack-1x fa-inverse"></i>
                         </span>
                         <h4 className="my-3">Responsive Design</h4>
                         <p className="text-muted">
@@ -34,7 +35,7 @@ function Services() {
                     </div>
                     <div className="col-md-4">
                         <span className="fa-stack fa-4x">
-                            <i className="fas fa-circle fa-stack-2x text-primary"></i>
+                            <i className="fas fa-circle fa-stack-2x text-danger"></i>
                             <i className="fas fa-lock fa-stack-1x fa-inverse"></i>
                         </span>
                         <h4 className="my-3">Web Security</h4>
